3f8107bc2f6023e868a71cbc1db438b27fb2eb99
gunawanzakiah <gunawanzakiah@gmail.com>
2020-06-14 08:56:20 +0700

    create file constant.php

---
+++ constant.php
@@ -0,0 +1,38 @@
+<?php
+
+class Coba
+{
+  public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+
+
+//function coba()
+//{
+//  return __FUNCTION__;
+//}
+
+//echo coba();
+
+///echo __DIR__;
+//echo __FILE__;
+//echo __LINE__;
+
+
+//class Coba
+//{
+//  const NAMA = 'Gunawan';
+//}
+
+//echo Coba::NAMA;
+
+
+//define('NAMA', 'Gunawan');
+//echo NAMA;
+
+//echo "<br>";
+
+//const UMUR = 32;
+//echo UMUR;
